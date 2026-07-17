3a3e20305ba9f750bb3c581e7ba711b02c063e5e
Pramod Kumar <pramod.kumar20100310@gmail.com>
2026-07-17 20:35:14 +0200

    fixes

---
+++ 02_FwHal/build/arm64/uiox_fw_main.d
@@ -0,0 +1,28 @@
+build/arm64/uiox_fw_main.o: src/uiox_fw_main.c include/uiox_fw_hw.h \
+ include/uiox_fw_uart.h include/uiox_fw_hw.h include/uiox_fw_timer.h \
+ include/uiox_fw_gpio.h include/uiox_fw_wdt.h include/uiox_fw_storage.h \
+ include/uiox_fw_net.h include/uiox_fw_sensor.h include/uiox_fw_thermal.h \
+ include/uiox_fw_pmic.h include/uiox_fw_bms.h include/uiox_fw_charger.h \
+ include/uiox_fw_usb.h include/uiox_fw_wifi.h include/uiox_fw_bt.h \
+ include/uiox_fw_monitor.h include/uiox_fw_touchpwd.h \
+ include/uiox_fw_devsw.h include/uiox_fw_fan.h
+include/uiox_fw_hw.h:
+include/uiox_fw_uart.h:
+include/uiox_fw_hw.h:
+include/uiox_fw_timer.h:
+include/uiox_fw_gpio.h:
+include/uiox_fw_wdt.h:
+include/uiox_fw_storage.h:
+include/uiox_fw_net.h:
+include/uiox_fw_sensor.h:
+include/uiox_fw_thermal.h:
+include/uiox_fw_pmic.h:
+include/uiox_fw_bms.h:
+include/uiox_fw_charger.h:
+include/uiox_fw_usb.h:
+include/uiox_fw_wifi.h:
+include/uiox_fw_bt.h:
+include/uiox_fw_monitor.h:
+include/uiox_fw_touchpwd.h:
+include/uiox_fw_devsw.h:
+include/uiox_fw_fan.h:
