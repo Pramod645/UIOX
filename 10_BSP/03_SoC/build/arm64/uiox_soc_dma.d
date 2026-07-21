8b79b23beded43478891ee371db921bc1fe0c845
Pramod Kumar <pramod.kumar20100310@gmail.com>
2026-07-21 20:17:38 +0200

    dnamic BSP or secondery bootloader update build fix

--- 10_BSP/03_SoC/build/arm64/uiox_soc_dma.d
+++
@@ -1,6 +0,0 @@
-build/arm64/uiox_soc_dma.o: src/uiox_soc_dma.c \
- src/../include/uiox_soc_dma.h src/../include/uiox_soc_types.h \
- src/../include/uiox_base_types.h
-src/../include/uiox_soc_dma.h:
-src/../include/uiox_soc_types.h:
-src/../include/uiox_base_types.h:
--- 10_BSP/03_SoC/build/arm64/uiox_soc_hw.d
+++
@@ -1,5 +0,0 @@
-build/arm64/uiox_soc_hw.o: src/uiox_soc_hw.c include/uiox_soc_hw.h \
- include/uiox_soc_types.h include/uiox_base_types.h
-include/uiox_soc_hw.h:
-include/uiox_soc_types.h:
-include/uiox_base_types.h:
